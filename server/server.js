55326583d88d94d1f13d3a0f403b451f2a236f2f
Devin Fischer <devinfischer32@gmail.com>
2021-09-17 16:48:54 -0600

    added database of words in backend

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const path = require('path')
 
-const app = express()
 
+const words = require('./words')
+
+const app = express()
+app.use(express.json())
+
+//words
+app.get('/words',(req,res)=>{
+    let selectedWord = words.wordArr[Math.floor(Math.random() * words.wordArr.length)]
+    res.status(200).send(selectedWord)
+})
+
+//main screen endpoint
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"../client/index.html"))
 })
 
+//css
 app.use("/css", express.static(path.join(__dirname, "../client/style.css")))
-
+//js
 app.use("/js", express.static(path.join(__dirname, "../client/index.js")))
 
+
+//heroku port
 const port = process.env.PORT || 4000
 
 app.listen(port, ()=>{
---
+++ server/words.js
@@ -0,0 +1,363 @@
+module.exports = {
+    wordArr: [
+    {
+        gamePhrase: "A JAR OF PENNIES",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "AIR FRESHENER",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "ANTIQUE VASE",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "BARBECUE GRILL",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "CORKSCREW",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "GARDEN TOOLS",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "JEWELRY BOX",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "LIGHT BULB",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "MAKEUP BRUSH",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "REMOTE CONTROL",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "SEWING KIT",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "SALAD BOWL",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "TACKLE BOX",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "TRASH CAN",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "WINEGLASSES",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "WOODEN DOOR",
+        type: "Around the House"
+    },
+    {
+        gamePhrase: "A LIGHT TOUCH",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "A JOYFUL SEASON",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "A LOUD VOICE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "A STROKE OF LUCK",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "A TRIP OVERSEAS",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "ABOVE AVERAGE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "ALUMINUM BASEBALL BAT",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "AMERICAN FLAG",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "AN OCEAN VIEW",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "BLT ON WHOLE WHEAT",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "CHINESE CALENDAR",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "CLASSIC ARCADE GAMES",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "CLEAN ENVIRONMENT",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "CLOGGED PIPE",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "COLLEGE DEGREE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "COLORFUL UMBRELLAS",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "FAST-FOOD RESTAURANTS",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "HEMP CLOTHING",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "HIGH DEFINITION",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "HIGH ROLLERS CLUB",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "HIGHER EDUCATION",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "HISTORICAL SIGNIFICANCE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "HOT DESERT CLIMATE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "IMAX THEATER",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "IMPULSE BUY",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "LANGUAGE TRANSLATOR",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "SALAD BOWL",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "SAND DUNES",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "SCIENCE PROJECT",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "SHIPMENT DATE",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "STOCK DIVIDENDS",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "STORMY CONDITIONS",
+        type: "Thing"
+    }, 
+    {
+        gamePhrase: "SUBWAY",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "WOODEN SLED",
+        type: "Thing"
+    },
+    {
+        gamePhrase: "ONCE UPON A TIME",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "SHIVER ME TIMBERS",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "THERE SHE BLOWS",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "YOU TALKIN TO ME",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "YOU CANT HANDLE THE TRUTH",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "I'LL BE BACK",
+        type: "Quotation"
+    },
+    {
+        gamePhrase: "AMAZON RIVER",
+        type: "Location"
+    },
+    {
+        gamePhrase: "BIG BEN",
+        type: "Location"
+    },
+    {
+        gamePhrase: "BIG TREE PARK",
+        type: "Location"
+    },
+    {
+        gamePhrase: "BROOKLYNN BRIDGE",
+        type: "Location"
+    },
+    {
+        gamePhrase: "HOLLYWOOD",
+        type: "Location"
+    },
+    {
+        gamePhrase: "HOOVER DAM",
+        type: "Location"
+    },
+    {
+        gamePhrase: "SAN FRANCISO",
+        type: "Location"
+    },
+    {
+        gamePhrase: "SYDNEY TOWER",
+        type: "Location"
+    },
+    {
+        gamePhrase: "THE EVERGLADES",
+        type: "Location"
+    },
+    {
+        gamePhrase: "THE PARTHENON",
+        type: "Location"
+    },
+    {
+        gamePhrase: "TIMES SQUARE",
+        type: "Location"
+    },
+    {
+        gamePhrase: "WALT DISNEY WORLD",
+        type: "Location"
+    },
+    {
+        gamePhrase: "YOSEMITE FALLS",
+        type: "Location"
+    },
+    {
+        gamePhrase: "ZION NATIONAL PARK",
+        type: "Location"
+    },
+    {
+        gamePhrase: "ACE VENTURA",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "ACHILLES",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "ARAGORN",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "BATMAN",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "BUGS BUNNY",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "CAPTAIN KIRK",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "CINDERELLA",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "DARTH VADER",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "DOCTOR DOLITTLE",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "KING ARTHUR",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "MILES MORALES",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "SANDMAN",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "SCOOBY DOO",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "SUPERMAN",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "THE FLINTSTONES",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "WONDER WOMAN",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "ZEUS",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "YOSEMITE SAM",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "TOUCAN SAM",
+        type: "Fictional Character"
+    },
+    {
+        gamePhrase: "TOM AND JERRY",
+        type: "Fictional Character"
+    }
+]}
