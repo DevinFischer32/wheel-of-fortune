580f3d072d2f1b00d888062b4601185c6fdfd553
Devin Fischer <devinfischer32@gmail.com>
2021-09-18 22:46:26 -0600

    fixing generator

--- client/index.js
+++ client/index.js
@@ -29,7 +29,7 @@ function startGame (e){
         for(let i=0; i<gamePhrase.length;i++){
            console.log('for-loop',gamePhrase[i])
            let newDiv = document.createElement('div').classList.add('hidden')
-           newDiv.textContent = gamePhrase[i]
+           newDiv.textContent = gamePhrase[i].value
            gameWord.appendChild(newDiv)
             // gameWord.textContent += gamePhrase[i]
             // gameWord.textContent += gamePhrase[i].value
