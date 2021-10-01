2c27351a9a864d40e549bc3150fa64a1f8648e7f
Devin Fischer <devinfischer32@gmail.com>
2021-10-01 09:57:30 -0600

    added logic

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 document.getElementById('userBtn').addEventListener('click', userName) 
-document.getElementById('guessBtn').addEventListener('click', playerGuess)
+
 // document.getElementById('vowelBuyBtn').addEventListener('click', bankAccount)
 document.getElementById('startBtn').addEventListener('click', startGame)
 const board = document.getElementById('gameBoard')
@@ -7,94 +7,74 @@ const board = document.getElementById('gameBoard')
 //var for word user is guessing
 let gameWord = document.getElementById('wordPhrase')
 let wordType = document.getElementById('wordType')
-console.log(gameWord)
-console.log(typeof gameWord)
 
 
-const baseUrl = 'https://wheel-0f-fortune.herokuapp.com' //for heroku link
-
-
+// const baseUrl = 'https://wheel-0f-fortune.herokuapp.com' //for heroku link
+const baseUrl = 'http://localhost:4000'
 
 
 function startGame (e){
-  //removes start screen
     e.target.parentNode.remove()
-
+    let idNum = 0
     //Need to get word from api
     axios.get(baseUrl + "/words").then((res)=>{
         let {gamePhrase, type} = res.data
-        console.log(gamePhrase, type)
         wordType.textContent = type
         gamePhrase = gamePhrase.split('')
         for(let i=0; i<gamePhrase.length;i++){
-           console.log('for-loop',gamePhrase[i])
-           let newDiv = document.createElement('div').classList.add('hidden')
-           newDiv.value = gamePhrase[i].value
-           gameWord.appendChild(newDiv)
-            // gameWord.textContent += gamePhrase[i]
-            gameWord.textContent += gamePhrase[i].value
+           let newDiv = document.createElement('div')
+           newDiv.textContent = gamePhrase[i]
+           newDiv.classList.add('hidden','gameCard')
+           newDiv.setAttribute(`id`, `grid_${idNum}`)   //adds a id to each div with a different number
+           gameWord.appendChild(newDiv)  //links the newDiv to 
+           idNum++  //number increases by 1 
         }
     })
-        
 }
-
-
 
 
 
 function userName(e){
     e.preventDefault()     
     document.getElementById('nameFeild').textContent = document.getElementById('name').value 
-    removeUserNameInput()
-}
-
-function removeUserNameInput (){
     let input = document.getElementById('name') 
     let button = document.getElementById('userBtn')
     input.remove()
     button.remove()
+   
+    
 }
 
-function playerGuess(e){
-    e.preventDefault()
-    const allGuess = document.getElementById('allGuess')
-    let playerInput = document.getElementById('playerGuess').value.toLowerCase()
-    let bankAccount = document.getElementById('bank')
-    
-    //matching variables 
-    let consonants = playerInput.match(/[bcdfghjklmnpqrstvwxyz]/g)
-    let vowels = playerInput.match(/[aeiou]/g)
-    
-    //matching for gamePhrase
-    
+let alphaBtn = document.getElementsByClassName('alphabet')
+let playerGuess
 
-    let bank = Number(bankAccount) //Need to set up buying vowels and adding money when right
-    bank = 0
+//block gets button value 
+for (let i in Object.values(alphaBtn) ) {
+    alphaBtn[i].onclick = function() {
+        playerGuess = this.value ;
+        checkGuesstoPhrase()
+    };
+}
 
-    let allow = false //allowing to use a vowel
+let bankAccount = document.getElementById('bankAccount').textContent
 
-    if(consonants){
-        allGuess.textContent += playerInput + " "
-        bank += 250
-    }else if(vowels){
-        if(allow != false && bank >= 250*vowels.length){
-            function buyingVowel(){
-                
-                allGuess.textContent += playerInput + " "
-            }
+function checkGuesstoPhrase(){
+    
+    if(playerGuess.match(/[AEIOU]/g)){
+        if(bankAccount >= 250){
+            alert('You Bought a Vowel')
+            bankMinus()
         }else{
-            alert('You need to purchase a vowel')
+            alert('Not enough bankroll')
         }
-    }else{
-        alert('Please Enter A Letter')
     }
-        //need to retrict vowels
-        //need a if no match to gamePhrase then allGuess.textContent += playerInput + " "
-    
-    document.getElementById('playerGuess').value = ''
 }
-// function bankAccount(){
-    
-// }
 
+function bankMinus(){
+    document.getElementById('bankAccount').textContent = bankAccount -= 250
+    return bankAccount
+}
 
+function bankAdd(){
+
+}
